dfb33bb59045bf5eb827984cbbe6b826c83a9d82
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-07-02 10:54:41 +0300

    Pages structure
    - installed react-router-dom
    - created basic pages tructure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+// Main application component
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import all pages
+import Home from "./pages/Home";
+import Hotel from "./pages/Hotel";
+import HotelsList from "./pages/HotelsList";
+
 function App() {
-  return <div className="App">Travel Agency</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Travel Agency</h1>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotels" element={<HotelsList />} />
+          <Route path="/hotels/:id" element={<Hotel />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/HotelsList.js
@@ -0,0 +1,3 @@
+export default function HotelsList() {
+  return <div>Hotels List</div>;
+}
