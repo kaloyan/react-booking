d805e8f4a7f9e2a3b22b000b163cc0e3f95ce0f3
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-07-10 08:49:36 +0300

    Created HotelsList page component
    - represent search results

--- src/pages/HotelsList.js
+++ src/pages/HotelsList.js
@@ -1,5 +1,40 @@
+import styles from "./HotelsList.module.css";
+
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
 import Header from "../components/Header";
+import SearchTool from "../components/search/SearchTool";
+import SearchItem from "../components/SearchItem";
+import Footer from "../components/footer/Footer";
 
 export default function HotelsList() {
-  return <Header compact={true} />;
+  const location = useLocation();
+  // console.log(location);
+
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
+
+  return (
+    <>
+      <Header compact={true} />
+
+      <div className={styles.listContainer}>
+        <div className={styles.wrapper}>
+          <div className={styles.results}>
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </div>
+
+          <SearchTool />
+        </div>
+      </div>
+
+      {/* <Footer /> */}
+    </>
+  );
 }
