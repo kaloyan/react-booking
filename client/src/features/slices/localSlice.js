63223d2745d53d1b4d410b3623e7864d0e34228a
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-07-22 07:09:32 +0300

    Created localSlice reducer
    - created new redux slice for saving favorites and managing system
      messages

---
+++ client/src/features/slices/localSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  favorites: [],
+  messages: [],
+};
+
+export const localSlice = createSlice({
+  name: "local",
+  initialState,
+  reducers: {
+    getFavorites: (state) => {
+      try {
+        const favString = localStorage.getItem("bookink_favs");
+
+        if (favString) {
+          state.favorites = JSON.parse(favString);
+        } else {
+          state.favorites = [];
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+
+    addFavorite: (state, action) => {
+      state.favorites.push(action.payload);
+      localStorage.setItem("bookink_favs", JSON.stringify(state.favorites));
+    },
+
+    removeFavorite: (state, action) => {
+      state.favorites = state.favorites.filter((x) => x.id !== action.payload);
+      localStorage.setItem("bookink_favs", JSON.stringify(state.favorites));
+    },
+
+    pushMessage: (state, action) => {
+      state.messages.push(action.payload);
+    },
+
+    removeMessage: (state, action) => {
+      state.messages = state.messages.filter((x) => x.id !== action.payload);
+    },
+  },
+});
+
+export const {
+  getFavorites,
+  addFavorite,
+  removeFavorite,
+  pushMessage,
+  removeMessage,
+} = localSlice.actions;
+
+export default localSlice.reducer;
--- client/src/features/store.js
+++ client/src/features/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import filterReducer from "./slices/filterSlice";
 import accountSlice from "./slices/accountSlice";
+import localSlice from "./slices/localSlice";
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
     account: accountSlice,
+    local: localSlice,
   },
 });
