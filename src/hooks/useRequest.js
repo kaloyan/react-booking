9d994450be2e98db9959ba8035dc2a3d70040464
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-07-30 21:26:52 +0300

    Implemented useRequest custom hook

---
+++ src/hooks/useRequest.js
@@ -0,0 +1,71 @@
+// Network request hook
+
+import { useState, useEffect } from "react";
+import * as netRequests from "../services/netRequest";
+import { useDispatch } from "react-redux";
+import { setSpinner } from "../features/slices/localSlice";
+import { setResponse, delResponse } from "../features/slices/responsesSlice";
+import { pushMessage } from "../features/slices/localSlice";
+
+const delay = 500;
+
+export const useRequest = (module, handle) => {
+  const [request, setRequest] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!request) return;
+
+    const beginRequest = true;
+
+    setTimeout(() => {
+      if (beginRequest) {
+        dispatch(setSpinner(true));
+      }
+    }, delay);
+
+    const [f, resolve, reject, ...params] = request;
+    const func = netRequests[module][f];
+
+    func(...params)
+      .then((response) => {
+        if (response?.response?.status) {
+          reject(false);
+          // throw error
+          throw {
+            error: response.response.data.message || "Server error",
+          };
+        } else {
+          dispatch(setResponse({ handle, data: response }));
+        }
+      })
+      .catch((err) => {
+        dispatch(pushMessage({ text: err.error, type: "error" }));
+        // clean
+        setRequest(null);
+        beginRequest = false;
+        reject(false);
+        dispatch(setSpinner(false));
+      });
+
+    setRequest(null);
+    beginRequest = false;
+    resolve(true);
+    dispatch(setSpinner(false));
+    // }
+  }, [request]);
+
+  const slice = {};
+
+  for (const func of Object.keys(netRequests[module])) {
+    slice[func] = (...params) => {
+      return new Promise((resolve, reject) => {
+        setRequest([func, resolve, reject, ...params]);
+      });
+    };
+  }
+
+  slice.cleaner = () => dispatch(delResponse(handle));
+
+  return slice;
+};
