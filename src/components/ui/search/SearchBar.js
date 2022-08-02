1ae1e47e9fb3a5c3f5784a8a37773caf4dd491f3
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-08-02 09:13:24 +0300

    Search component rework
    - rewriten SearchBar component from scratch to use URL queries

--- src/components/ui/search/SearchBar.js
+++ src/components/ui/search/SearchBar.js
@@ -1,103 +1,161 @@
-import styles from "./SearchBar.module.css";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  setDestination,
-  setStartDate,
-  setEndDate,
-} from "../../../features/slices/filterSlice";
+import { useSelector } from "react-redux";
+import { useFormik } from "formik";
 
+import styles from "./SearchBar.module.css";
+// import { useCatalog } from "../../../hooks/useCatalog";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faCaretUp,
   faCaretDown,
-  faCalendar,
 } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-import GuestsBox from "./GuestsBox";
-import { useLoadContent } from "../../../hooks/useLoadcontent";
 
 export default function SearchBar() {
   const navigate = useNavigate();
-  const loadContent = useLoadContent();
 
-  const { destination, startDate, endDate } = useSelector(
-    (state) => state.filter
-  );
-  const dispatch = useDispatch();
+  const [openOptions, setOpenOptions] = useState(false);
 
-  const [openSearch, setOpenSearch] = useState(false);
+  // const catalog = useCatalog();
+  const { filters } = useSelector((state) => state.local);
 
-  const searchHandler = (e) => {
-    e.preventDefault();
-    loadContent();
-    navigate("/catalog");
+  useEffect(() => {
+    formik.setValues({
+      type: filters.properyType,
+      price: filters.maxPrice,
+      rating: filters.minRating,
+      dest: filters.destination,
+    });
+  }, [filters]);
+
+  const formik = useFormik({
+    initialValues: {
+      type: filters.properyType,
+      price: filters.maxPrice,
+      rating: filters.minRating,
+      dest: filters.destination,
+    },
+
+    onSubmit: (values) => {
+      setOpenOptions(false);
+
+      const params = {};
+      if (values.dest) params.dest = values.dest;
+      if (values.rating) params.rating = values.rating;
+      if (values.type) params.type = values.type;
+      if (values.price) params.price = values.price;
+      params.limit = 8;
+      params.offset = 0;
+
+      const str = new URLSearchParams(params).toString();
+      const query = encodeURI(`${str}`);
+
+      navigate("/catalog/query?" + query);
+    },
+  });
+
+  const toggleOpenOptions = (e) => {
+    setOpenOptions((state) => !state);
   };
 
-  const toggleOpenSearch = (e) => {
-    setOpenSearch((state) => !state);
-  };
-
-  const setDateStart = (e) => {
-    dispatch(setStartDate(e.target.value));
-  };
-
-  const setDateEnd = (e) => {
-    dispatch(setEndDate(e.target.value));
+  const clearHandler = () => {
+    formik.setValues({
+      type: "",
+      price: "",
+      rating: "",
+      dest: "",
+    });
   };
 
   return (
-    <form
-      className={`${styles["search-tool"]} ${openSearch && styles["visible"]}`}
-      onSubmit={searchHandler}
-    >
+    <form className={`${styles["search-tool"]}`} onSubmit={formik.handleSubmit}>
       <div className={styles["container"]}>
-        <button
-          type="button"
-          onClick={toggleOpenSearch}
-          className={styles["close-btn"]}
-        >
-          {openSearch ? (
-            <FontAwesomeIcon icon={faCaretUp} />
-          ) : (
-            <FontAwesomeIcon icon={faCaretDown} />
-          )}
-        </button>
-
         <input
           type="search"
-          className={styles["search-box"]}
+          name="dest"
+          className={`${styles["search-box"]} ${
+            openOptions && styles["active"]
+          }`}
           placeholder="Where do you want to go today?"
-          value={destination}
-          onChange={(e) => dispatch(setDestination(e.target.value))}
+          value={formik.values.dest}
+          onChange={formik.handleChange}
         />
 
         <button type="submit" className={styles["search-btn"]}>
-          <FontAwesomeIcon icon={faMagnifyingGlass} /> {openSearch && "Search"}
+          <FontAwesomeIcon icon={faMagnifyingGlass} /> {openOptions && "Search"}
         </button>
+
+        <div className={styles["fiter-btn"]} onClick={toggleOpenOptions}>
+          <button type="button" className={styles["close-btn"]}>
+            {openOptions ? (
+              <FontAwesomeIcon icon={faCaretUp} />
+            ) : (
+              <FontAwesomeIcon icon={faCaretDown} />
+            )}
+          </button>
+
+          {!openOptions && <div>options</div>}
+        </div>
       </div>
 
       <div
-        className={`${styles["wrapper"]} ${openSearch && styles["visible"]}`}
+        className={`${styles["wrapper"]} ${openOptions && styles["visible"]}`}
       >
-        <div className={styles["date-box"]}>
-          <h3>
-            <FontAwesomeIcon icon={faCalendar} /> Arrivals
-          </h3>
+        <div className={styles["filter-box"]}>
+          <div>
+            <label htmlFor="type">Property Type</label>
 
-          <input type="date" value={startDate} onChange={setDateStart} />
+            <select
+              name="type"
+              id="type"
+              value={formik.values.type}
+              onChange={formik.handleChange}
+            >
+              <option value="">-- no set --</option>
+              <option value="hotel">Hotel</option>
+              <option value="resort">Resort</option>
+              <option value="villa">Villa</option>
+              <option value="apartment">Apartment</option>
+              <option value="cabin">Cabin</option>
+            </select>
+          </div>
 
-          <h3>
-            <FontAwesomeIcon icon={faCalendar} /> Leaving
-          </h3>
-          <input type="date" value={endDate} onChange={setDateEnd} />
+          <div>
+            <label htmlFor="rating">Minimal Rating</label>
+
+            <select
+              name="rating"
+              id="rating"
+              value={formik.values.rating}
+              onChange={formik.handleChange}
+            >
+              <option value="">-- not set --</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="price">Max Price</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+            />
+          </div>
+
+          <div>
+            <button type="button" onClick={clearHandler}>
+              Clear All
+            </button>
+          </div>
         </div>
-
-        <GuestsBox />
       </div>
-
-      <div className={styles.searchItem}></div>
     </form>
   );
 }
