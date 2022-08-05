2df2916f2433110757b1291a8c23ea91df53dca3
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-08-05 16:15:26 +0300

    Implemented rooms listings in Hotel component
    - minor styles bugfixes

--- src/components/pages/hotel/RoomsList.js
+++ src/components/pages/hotel/RoomsList.js
@@ -3,12 +3,31 @@ import styles from "./RoomsList.module.css";
 export default function RoomsList({ rooms }) {
   return (
     <div className={styles["container"]}>
-      <div>Available rooms:</div>
+      <div className={styles["title"]}>
+        <h2>Available rooms:</h2>
+      </div>
 
-      <ul>
-        {rooms.map((x) => (
-          <li key={x._id}>{x.title}</li>
-        ))}
+      <ul className={styles["room-list"]}>
+        {rooms?.length > 0 && (
+          <>
+            {rooms.map((x) => (
+              <li key={x._id}>
+                <div>{x.title}</div>
+                <div>{x.description}</div>
+                <div>room numbers: [{x.roomNumbers.join(", ")}]</div>
+                <div> price: ${x.price}</div>
+              </li>
+            ))}
+          </>
+        )}
+
+        {rooms?.length == 0 && (
+          <li>
+            <div className={styles["no-rooms"]}>
+              No available rooms at the moment
+            </div>
+          </li>
+        )}
       </ul>
     </div>
   );
