5d6f79eda2cd40f66d79df987f0e001b0e3f1672
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-08-11 11:21:31 +0300

    Newsletter functionality
    - added functionality for newsletter subscription

--- src/components/ui/Subscribtion.js
+++ src/components/ui/Subscribtion.js
@@ -1,23 +1,48 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { pushMessage } from "../../features/slices/localSlice";
 import styles from "./Subscribtion.module.css";
 
 export default function Subscribtion() {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Save time, save money!</h1>
+  const emailRef = useRef(null);
+  const dispatch = useDispatch();
 
-      <span className={styles.description}>
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+
+    dispatch(
+      pushMessage({
+        text: `${email} is now subscribed to our newsletter.`,
+        type: "success",
+      })
+    );
+
+    emailRef.current.value = "";
+  };
+
+  return (
+    <form className={styles["container"]} onSubmit={handleSubscribe}>
+      <h1 className={styles["title"]}>Subscribe to our newsletter</h1>
+
+      <span className={styles["description"]}>
         Sign up and we'll send the best deals to you
       </span>
 
-      <div className={styles.inputContainer}>
+      <div className={styles["input-container"]}>
         <input
           type="email"
+          required
           className={styles.emailInput}
           placeholder="Your email"
+          ref={emailRef}
         />
 
-        <button className={styles.subscribeBtn}>Subscribe</button>
+        <button className={styles["subscribe-btn"]} type="submit">
+          Subscribe
+        </button>
       </div>
-    </div>
+    </form>
   );
 }
